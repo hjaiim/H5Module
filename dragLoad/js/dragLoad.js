c9adee4f7affbc6628acf97adceffcbe838177fd
lihuazhai <liquorbar@163.com>
2016-02-02 19:09:13 +0800

    add callbackFun
    增加回调

--- dragLoad/js/dragLoad.js
+++ dragLoad/js/dragLoad.js
@@ -7,14 +7,22 @@
     var DragLoad = function (ele, opt) {
         this.element = ele;
         this.defaults = {
-            from : 0,
+			fromField: "from",
+			currentPageField: "currentPage",
+			currentPage : 2,
+			pageCountField: "pageCount",
             pageCount : 10,
             //配置加载提示dom
             loadingDom:$('.pageLoading'),
             //延迟显示，即加载提示显示时间
-            delayTime:1000
+            delayTime:1000,
+			callbackFun :{
+				callbackFun:null,
+				formatData:null,
+				noMoreHandle:null
+			}
         };
-        this.opts = $.extend({}, this.defaults, opt)
+        this.opts = $.extend({}, this.defaults, opt);
         this.init();
     }
 
@@ -23,6 +31,8 @@
          *绑定滚动事件
          */
         init: function () {
+			//第一次起始数据条数
+			this.opts.from = this.opts.pageCount;
             var dragThis = this;
             $(window).scroll(function () {
                 //已经滚动到上面的页面高度
@@ -37,18 +47,27 @@
                 }
             });
         },
+		//判断是否是回调函数
+		toolsIsFun: function(fun){
+			var flag = false;
+			if( typeof fun === 'function' ){
+				flag = true;
+			}
+			return flag;
+		},
         /*
          *获取下一页的数据
          */
         getData: function () {
             var dragThis = this;
+			var data = {};
+			data[dragThis.opts.fromField] = dragThis.opts.from;
+			data[dragThis.opts.currentPageField] = dragThis.opts.currentPage;
+			data[dragThis.opts.pageCountField] = dragThis.opts.pageCount;
             $.ajax({
                 type: 'GET',
                 url: dragThis.opts.url,
-                data: {
-                    from: dragThis.opts.from,
-                    pageCount: dragThis.opts.pageCount
-                },
+                data: data,
                 dataType: 'json',
                 timeout: 5000,
                 beforeSend: function(){
@@ -57,10 +76,19 @@
                 },
                 success: function (data) {
                     //console.info(data);
+					//数据回调处理，返回纯数据
+					if( typeof dragThis.opts.callbackFun.formatData === 'function' ){
+						data = dragThis.opts.callbackFun.formatData(data);
+					}
+					
                     if(data != null){
                         var t = setTimeout(function(){dragThis.render(data);},dragThis.opts.delayTime);
                     }else{
-                        dragThis.opts.loadingDom.html("没有更多");
+						if(dragThis.toolsIsFun(dragThis.opts.callbackFun.noMoreHandle)){
+							dragThis.opts.callbackFun.noMoreHandle();
+						}else{
+							dragThis.opts.loadingDom.html("没有更多");
+						}
                     }
                 },
                 error: function (xhr, type) {
@@ -82,11 +110,18 @@
             //隐藏加载提示
             this.opts.loadingDom.css("visibility","hidden");
             this.element.append(dom);
-            this.opts.from = this.opts.from + this.opts.pageCount;
+            this.handlePage();
         },
         insertDom: function () {
             var data = this.getData();
         },
+		handlePage : function(){
+			this.opts.currentPage = this.opts.currentPage + 1;
+			this.opts.from = this.opts.from + this.opts.pageCount;
+		},
+		getCurrentPage : function(){
+			return this.currentPage;
+		},
         returnOjb: function () {
             return this.element;
         }
